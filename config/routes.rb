d615722b195ecbf7a2e64acdbafeb025412d2438
Isabella Gonzales <isabella.ferzales@gmail.com>
2025-10-20 21:08:59 -0300

    Base inicial do projeto: estrutura de models, controllers, views e configurações

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :movies do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  # Rotas do Active Storage já estão automáticas
+  # Ex.: rails_blob_path, rails_blob_url, rails_direct_uploads_path
+
+  root "movies#index"
+end
